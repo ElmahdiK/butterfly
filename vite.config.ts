c8b2f211297c877d48bb463761489409c019ca89
ElmahdiK <elmahdi.korfed@gmail.com>
2025-03-20 20:11:08 +0100

    fix: config base updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/guide/static-deploy
 export default defineConfig({
   plugins: [react()],
-  base: 'https://elmahdik.github.io/butterfly/',
+  base: '/',
   build: {
     outDir: 'dist',
   },
