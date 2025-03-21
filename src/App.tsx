a4c545f4e7c24e7d612093eead84e5ae390340b2
ElmahdiK <elmahdi.korfed@gmail.com>
2025-03-21 10:51:22 +0100

    fix: replace no results image format from webp to jpg

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ const App = () => {
           />
         ))}
         {filteredButterflies.length === 0 && (
-          <p className="no-results">No results found for " {searchText} " <img src="./img/_noresults.webp" /></p>
+          <p className="no-results">No results found for " {searchText} " <img src="./img/_noresults.jpg" /></p>
         )}
       </main>
     </>
