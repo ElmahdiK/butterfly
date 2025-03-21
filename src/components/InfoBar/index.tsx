3a88cc675c036a96e0926473875d6e9fa13efdc3
ElmahdiK <elmahdi.korfed@gmail.com>
2025-03-21 10:23:09 +0100

    feat : infoBar added

---
+++ src/components/InfoBar/index.tsx
@@ -0,0 +1,19 @@
+import "./style.scss";
+
+const InfoBar = () => {
+  return (
+    <div id="div-info-bar">
+      <span>Le saviez-vous ? 🦋</span>
+      <p>
+        Les papillons ont un goût... au bout de leurs pieds ! En effet, leurs
+        récepteurs gustatifs se trouvent sur leurs pattes, leur permettant de
+        "goûter" les plantes sur lesquelles ils se posent, pour vérifier si
+        elles sont adaptées à la ponte de leurs œufs.
+        <br />
+        C’est une manière unique et fascinante de percevoir leur environnement !
+      </p>
+    </div>
+  );
+};
+
+export default InfoBar;
