fc6d822ba42f375d39ba2950dab7c0915a704a58
ElmahdiK <elmahdi.korfed@gmail.com>
2025-03-22 12:08:36 +0100

    fix: update routing paths and navigation data for consistency and deployment

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,8 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Nav />}>
       <Route index element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="shop" element={<Shop />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/shop" element={<Shop />} />
       <Route path="*" element={<NotFound />} />
     </Route>
   )
