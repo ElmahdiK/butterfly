ba578b00fe039fa205d933d0e1b1ecf00678c92e
ElmahdiK <elmahdi.korfed@gmail.com>
2025-03-22 11:56:05 +0100

    feat: update routing structure to use createRoutesFromElements and add homepage for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import "./styles/App.scss";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import About from "./pages/About";
@@ -9,33 +14,19 @@ import Nav from "./components/Nav";
 
 // https://medium.com/@galohernandez/vite-react-react-router-dom-the-latest-way-312ee887197e
 const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/about",
-      element: <About />,
-    },
-    {
-      path: "/shop",
-      element: <Shop />,
-    },
-    {
-      path: "*",
-      element: <NotFound />,
-    },
-  ],
-  {
-    basename: "/butterfly", // Ajout du basename ici
-  }
+  createRoutesFromElements(
+    <Route path="/" element={<Nav />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="shop" element={<Shop />} />
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
 );
 
 const App = () => {
   return (
     <>
-      <Nav />
       <RouterProvider router={router} />
     </>
   );
