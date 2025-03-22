d9b442a8d194086d369b7fc9f86208065fd8f91b
ElmahdiK <elmahdi.korfed@gmail.com>
2025-03-22 12:33:58 +0100

    fix: update base path for GitHub Pages deployment in router and Vite configuration

--- src/main.tsx
+++ src/main.tsx
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
     ],
   },
 ], {
-  basename: `https://elmahdik.github.io/butterfly`, // Cela ajoute le chemin de base correspondant à ton repo GitHub Pages
+  basename: `/butterfly`, // Cela ajoute le chemin de base correspondant à ton repo GitHub Pages
 });
 
 createRoot(document.getElementById("root")!).render(
