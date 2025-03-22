95d9b6a33eca894e1fc7451774a8d072f5380e98
ElmahdiK <elmahdi.korfed@gmail.com>
2025-03-22 12:23:15 +0100

    feat: refactor routing structure to use createBrowserRouter and simplify App component

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,21 +1,18 @@
 import "./style.scss";
-import { Outlet, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import LINKS from "../../assets/data/nav.json";
 
 export default function Nav() {
   return (
-    <>
-      <nav>
-        <p>Butterfly</p>
-        <ul>
-          {LINKS.map((link) => (
-            <li key={link.path}>
-              <Link to={link.path}>{link.name}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <Outlet />
-    </>
+    <nav>
+      <p>Butterfly</p>
+      <ul>
+        {LINKS.map((link) => (
+          <li key={link.path}>
+            <Link to={link.path}>{link.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/index.scss'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/index.scss";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
-createRoot(document.getElementById('root')!).render(
+// 
+// https://medium.com/@galohernandez/vite-react-react-router-dom-the-latest-way-312ee887197e
+// https://www.youtube.com/watch?v=SOwKHCwfj08&ab_channel=Hackmyhead
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/about", element: <About /> },
+      { path: "/shop", element: <Shop /> },
+      { path: "/*", element: <NotFound /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
