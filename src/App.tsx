925e77f8b539e18d78b3a26cf3ae35f9c5700b5f
ElmahdiK <elmahdi.korfed@gmail.com>
2025-03-22 12:23:15 +0100

    feat: refactor routing structure to use createBrowserRouter and simplify App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,12 @@
+import { Outlet } from "react-router-dom";
 import "./styles/App.scss";
-
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-} from "react-router-dom";
-import Home from "./pages/Home";
-import Shop from "./pages/Shop";
-import About from "./pages/About";
-import NotFound from "./pages/NotFound";
 import Nav from "./components/Nav";
 
-// https://medium.com/@galohernandez/vite-react-react-router-dom-the-latest-way-312ee887197e
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Nav />}>
-      <Route index element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/shop" element={<Shop />} />
-      <Route path="*" element={<NotFound />} />
-    </Route>
-  )
-);
-
-const App = () => {
+export default function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      <Nav />
+      <Outlet />
     </>
   );
-};
-
-export default App;
+}
