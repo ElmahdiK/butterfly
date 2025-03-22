953e43cec7c07442ee9f8a81a0fb3056a309d533
ElmahdiK <elmahdi.korfed@gmail.com>
2025-03-22 11:51:16 +0100

    feat: refactor routing structure to use array format and add basename for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import "./styles/App.scss";
 
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import About from "./pages/About";
@@ -14,20 +9,33 @@ import Nav from "./components/Nav";
 
 // https://medium.com/@galohernandez/vite-react-react-router-dom-the-latest-way-312ee887197e
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Nav />}>
-      <Route index element={<Home />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/shop" element={<Shop />} />
-      <Route path="*" element={<NotFound />} />
-    </Route>
-  )
+  [
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/about",
+      element: <About />,
+    },
+    {
+      path: "/shop",
+      element: <Shop />,
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+  ],
+  {
+    basename: "/butterfly", // Ajout du basename ici
+  }
 );
 
 const App = () => {
   return (
     <>
+      <Nav />
       <RouterProvider router={router} />
     </>
   );
