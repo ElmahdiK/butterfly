9ec74d817c654fee4cd38181a1e4a233e0ff0633
ElmahdiK <elmahdi.korfed@gmail.com>
2025-03-24 06:54:38 +0100

    refactor: update styles in Header and Nav components; add gradient background to body

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -4,19 +4,17 @@ import LINKS from "../../assets/data/nav.json";
 
 export default function Nav() {
   return (
-    <>
-      <nav>
-        <p>
-          <Link to={`/`}>Butterfly</Link>
-        </p>
-        <ul>
-          {LINKS.map((link) => (
-            <li key={link.path}>
-              <Link to={link.path}>{link.name}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </>
+    <nav>
+      <p>
+        <Link to={`/`}>Butterfly</Link>
+      </p>
+      <ul>
+        {LINKS.map((link) => (
+          <li key={link.path}>
+            <Link to={link.path}>{link.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 }
